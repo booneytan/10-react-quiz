eac24fdc0cc5d7addbe8f7bbe3c1fa61a4e0b9b1
booneytan <73635818+booneytan@users.noreply.github.com>
2024-06-20 11:11:06 +0800

    Setup a timer for quiz

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,8 +1,19 @@
-function NextButton({dispatch, answer}) {
-    if(answer === null) return null
+function NextButton({ dispatch, answer, index, numQuestions }) {
+  if (answer === null) return null;
+
+  if (index < numQuestions - 1)
     return (
-        <button className="btn btn-ui" onClick={()=>dispatch({type: 'nextQuestion'})}>Next</button>
-    )
+      <button className="btn btn-ui" onClick={() => dispatch({ type: "nextQuestion" })}>
+        Next
+      </button>
+    );
+
+  if (index === numQuestions - 1)
+    return (
+      <button className="btn btn-ui" onClick={() => dispatch({ type: "finish" })}>
+        Finish
+      </button>
+    );
 }
 
-export default NextButton
+export default NextButton;
